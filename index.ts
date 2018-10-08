823249de9011eee224c09d37a53b7b2a79b3c8d4
raminarmanfar <ramin.armanfar@msg.group>
2018-10-08 14:25:26 +0200

    second push

---
+++ index.ts
@@ -0,0 +1,3 @@
+export { MyServiceService } from './src/app/my-service.service';
+export { MyModuleModule } from './src/app/my-module/my-module.module';
+export { SimpComponent } from './src/app/simp/simp.component';
--- src/app/my-module/my-module.module.ts
+++ src/app/my-module/my-module.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SimpComponent } from '../simp/simp.component';
+import { MyServiceService } from '../my-service.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [SimpComponent],
+  providers: [MyServiceService],
+  exports: [SimpComponent]
 })
 export class MyModuleModule { }
